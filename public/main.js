7e32ed4ddafc89e979e9fe9d10c0d8343a46d0d4
Chandrika06 <115099376+Chandrika06@users.noreply.github.com>
2022-12-07 06:54:53 +0530

    Final Commit

--- public/main.js
+++ public/main.js
@@ -1,4 +1,102 @@
+async function fetchData(route = '', data = {}, methodType) {
+    const response = await fetch(`http://localhost:3000${route}`, {
+      method: methodType, // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    if(response.ok) {
+      return await response.json(); // parses JSON response into native JavaScript objects
+    } else {
+      throw await response.json();
+    }
+  }
+  
+  // user class
+  class User {
+    constructor(userName, password, fullName) {
+      this.userName = userName;
+      this.password = password;
+      this.fullName = fullName;
+    }
+  
+    getUsername() {
+      return this.userName;
+    }
+  }
+  
+  // login functionality
+  let loginForm = document.getElementById("login-page");
+  if(loginForm) loginForm.addEventListener('submit', login);
+  
+  function login(e) {
+    e.preventDefault();
+  
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let user = new User(userName, password);
+  
+    fetchData("/users/login", user, "POST")
+    .then((data) => {
+      setCurrentUser(data);
+      window.location.href = "Notes.html";
+    })
+    .catch((err) => {
+      console.log(`Error!!! ${err.message}`)
+    }) 
+  }
+   
+  // register functionality
+  let regForm = document.getElementById("register-Form");
+  if(regForm) regForm.addEventListener('submit', register);
+  
+  function register(e) {
+    e.preventDefault();
+  
+    let userName = document.getElementById("username").value;
+    let userfName = document.getElementById("fname").value;
+    let userlName = document.getElementById("lname").value;
+    let password = document.getElementById("password").value;
+    let user = new User(userName, userfName, userlName, password);
+  
+    fetchData("/users/register", user, "POST")
+    .then((data) => {
+      setCurrentUser(data);
+      window.location.href = "login.html";
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+  
+  // logout event listener
+  let logout = document.getElementById("logout-btn");
+  if(logout) logout.addEventListener('click', removeCurrentUser)
+  
+  // stateful mechanism for user
+  // logging in a user
+  function setCurrentUser(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+  
+  // getting current user function
+  // FIX this next class
+  function getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+  
+  // logout function for current user
+  function removeCurrentUser() {
+    localStorage.removeItem('user')
+  }
 
+/*
 class User{
     constructor(fname,lname,name,password,notes){
         this.userfName = fname;
@@ -94,7 +192,7 @@ function getNotes(){
 }
 
 
-/*
+
 const user_btn = document.getElementById("users-btn");
 if(user_btn) user_btn.addEventListener('click',getUsers);
 
